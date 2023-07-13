2eac5d8a45278ae2eb70b87624fbc113d7bd03a0
chengdaosen <2206164816@qq.com>
2023-07-13 23:07:10 +0800

    图库分类

--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -10,23 +10,56 @@
       </AsideList>
     </div>
     <div class="bottom">
-      <el-pagination background layout="prev,  next" :total="1000" />
+      <el-pagination
+        background
+        layout="prev,  next"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="limit"
+        @current-change="getData"
+      />
     </div>
   </el-aside>
+  <FormDrawer ref="formDrawerRef" @submit="handleSubmit" title="新增">
+    <el-form
+      :model="form"
+      ref="formRef"
+      :rules="rules"
+      label-width="80px"
+      :inline="false"
+    >
+      <el-form-item label="分类名称" prop="name">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="分类名称" prop="order">
+        <el-input-number v-model="form.order" :min="0" :max="1000" />
+      </el-form-item>
+    </el-form>
+  </FormDrawer>
 </template>
 <script setup>
+import FormDrawer from '@/components/FormDrawer.vue'
 import AsideList from './AsideList.vue'
 import { reqGetImageClassList } from '@/api/image_class'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const list = ref([])
 //加载动画
 const loading = ref(false)
 const activeId = ref(0)
+//分页
+const currentPage = ref(1)
+const total = ref(0)
+const limit = ref(10)
 //获取数据
-function getData() {
+function getData(p = null) {
+  if (typeof p == 'number') {
+    currentPage.value = p
+  }
+  console.log(p)
   loading.value = true
-  reqGetImageClassList(1)
+  reqGetImageClassList(currentPage.value)
     .then((res) => {
+      total.value = res.totalCount
       list.value = res.list
       let item = list.value[0]
       if (item) {
@@ -37,8 +70,34 @@ function getData() {
       loading.value = false
     })
 }
-
 getData()
+
+const formDrawerRef = ref(null)
+const handleCreate = () => formDrawerRef.value.open()
+
+const form = reactive({
+  name: '',
+  order: 50,
+})
+const rules = {
+  name: [
+    {
+      required: true,
+      message: '图库分类名称不能为空',
+      trigger: 'blur',
+    },
+  ],
+}
+const formRef = ref(null)
+const handleSubmit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return
+    console.log('提交成功')
+  })
+}
+defineExpose({
+  handleCreate,
+})
 </script>
 <style>
 .image-aside {
--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,12 +1,19 @@
 <template>
   <el-container class="bg-white rounded" :style="{ height: h + 'px' }">
-    <el-header class="image-header">Header</el-header>
-    <el-container><ImageAside /><ImageMain /></el-container>
+    <el-header class="image-header">
+      <el-button type="primary" size="small" @click="handleOpenCreate"
+        >图片分类</el-button
+      >
+    </el-header>
+    <el-container><ImageAside ref="ImageAsideRef" /><ImageMain /></el-container>
   </el-container>
 </template>
 <script setup>
 import ImageAside from '../../components/ImageAside.vue'
 import ImageMain from '../../components/ImageMain.vue'
+import { ref } from 'vue'
+const ImageAsideRef = ref(null)
+const handleOpenCreate = () => ImageAsideRef.value.handleCreate()
 const windowHeight = window.innerHeight || document.body.clientHeight //拿到浏览器可视区域高度
 const h = windowHeight - 64 - 44 - 40 //padding有40
 </script>
