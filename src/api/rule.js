0fb5b976fdbb1a2a186272c993bbf2ba9b7c459a
chengdaosen <2206164816@qq.com>
2023-07-24 15:27:51 +0800

    菜单权限功能

--- src/api/rule.js
+++ src/api/rule.js
@@ -3,3 +3,10 @@ import axios from '@/axios'
 export function getRuleList(page) {
   return axios.get(`/admin/rule/${page}`)
 }
+export function createRule(data) {
+  return axios.post('/admin/rule', data)
+}
+
+export function updateRule(id, data) {
+  return axios.post('/admin/rule/' + id, data)
+}
