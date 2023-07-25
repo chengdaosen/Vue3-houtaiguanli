46ebb2644f027e56e237389c0ff0fa893e0d6104
chengdaosen <2206164816@qq.com>
2023-07-25 15:26:56 +0800

    配置权限功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -119,6 +119,14 @@ const asyncRoutes = [
       title: '菜单权限管理',
     },
   },
+  {
+    path: '/role/list',
+    name: '/role/list',
+    component: () => import('@/pages/role/list.vue'),
+    meta: {
+      title: '角色管理',
+    },
+  },
 ]
 
 export const router = createRouter({
